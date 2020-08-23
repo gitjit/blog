818745ea195e179482a44663f8a61a4366d7bd40
Jithesh <jithesh.chandrasekharan@hp.com>
2020-08-23 16:34:18 -0700

    Updated code

--- cognito_auth/config.js
+++ cognito_auth/config.js
@@ -1,4 +1,4 @@
 var poolData = {
-    UserPoolId: "us-west-2_sEPh32SX1",
-    ClientId: "36fuoe8gvn33d8b5eth9qqbhc6",
+    UserPoolId: "us-west-2_******", // Please provide yours
+    ClientId: "36fuoe8gvn33d8b5et****", // Please provide yours
   };
