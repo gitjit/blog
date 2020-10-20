9de7889d04480dfb2cd82e8d1ea091d538cf1247
Jithesh <jithesh.chandrasekharan@hp.com>
2020-10-19 20:13:51 -0700

    edit gitignore

--- cloud_friends/js/config.js
+++
@@ -1,4 +0,0 @@
-var poolData = {
-    UserPoolId: "us-west-xxxxxx", // Please provide yours
-    ClientId: "4ckbanaxxxxxxxxxx", // Please provide yours
-  };
--- cognito_auth/config.js
+++
@@ -1,4 +0,0 @@
-var poolData = {
-    UserPoolId: "us-west-2_******", // Please provide yours
-    ClientId: "36fuoe8gvn33d8b5et****", // Please provide yours
-  };
